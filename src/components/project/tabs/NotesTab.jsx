a89a2bb0bfaad2116003754ed87c4df3726afc1d
grant reddy <grantreddy85@hotmail.com>
2026-02-17 23:15:08 +0000

    File changes

--- src/components/project/tabs/NotesTab.jsx
+++ src/components/project/tabs/NotesTab.jsx
@@ -206,8 +206,8 @@ export default function NotesTab({ project }) {
       </div>
 
       {/* Editor Panel */}
-      {isEditorOpen && (
-        <div className="flex-1 flex flex-col bg-[#fafbfc] min-w-0">
+       {isEditorOpen && (
+         <div className={`flex-1 flex flex-col bg-[#fafbfc] min-w-0 ${assistantOpen ? "relative" : ""}`}>
           {/* Editor Toolbar */}
           <div className="flex items-center justify-between px-6 py-3 border-b border-gray-100 bg-white">
             <div className="flex items-center gap-2">
