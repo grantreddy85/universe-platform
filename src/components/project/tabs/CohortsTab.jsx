8ba58b53e51dcab262349649e0f2200b0ca10ba2
grant reddy <grantreddy85@hotmail.com>
2026-02-23 10:19:39 +0000

    File changes

--- src/components/project/tabs/CohortsTab.jsx
+++ src/components/project/tabs/CohortsTab.jsx
@@ -3,6 +3,7 @@ import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Plus, FlaskConical, MoreHorizontal, Trash2, Sparkles } from "lucide-react";
 import TabAIPanel from "./TabAIPanel";
+import CohortFilters from "@/components/cohorts/CohortFilters";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import {
