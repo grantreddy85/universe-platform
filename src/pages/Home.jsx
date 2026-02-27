2b95b077ff10bfb184399b66e9829a9cef1acd6e
grant reddy <grantreddy85@hotmail.com>
2026-02-27 08:06:49 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -234,9 +234,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
-
-      </div>
-    </div>);
+        </div>
+        </div>
+        </div>
+        </div>);
 
 }
